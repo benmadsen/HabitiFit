da15f23de7f363845134c12e33560141411cfb21
Darius Lam <dariuslamikins@gmail.com>
2015-08-28 15:14:21 -0700

    initial commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+    require_once('habitica/habitica_api.php');
+    require_once('fitbit/fitbitphp.php');
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        
+    </head>
+    
+    <body>
+        
+    </body>
+</html>
