7d33fabdd61804c8a36009c0dbd5efe5e04e10be
Darius Lam <dariuslamikins@gmail.com>
2015-09-01 17:38:34 -0700

    minor changes to track code

--- index.php
+++ index.php
@@ -3,21 +3,10 @@
     <head>
         <title>FitBitica-2.2</title>
        <script src="js/jquery-2.1.4.min.js"></script>
-       <script src="js/index.js"></script>
        <script src="js/bootstrap.min.js"></script>
+        <script src="js/index.js"></script>
        <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css" />
        <link rel="stylesheet" href="css/style.css" type="text/css" />
-       
-       <script>
-          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-          })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-          ga('create', 'UA-52252320-2', 'auto');
-          ga('send', 'pageview');
-
-        </script>
        
     </head>
     
--- js/index.js
+++ js/index.js
@@ -7,6 +7,17 @@ $(document).ready(function() {
    //TO ADD:  Check to see if Fitbit auth token has expired to present a new auth form!
    //TO ADD: error handling!
     
+    
+   //Google analytics
+    
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-52252320-3', 'auto');
+      ga('send', 'pageview');
+    
    //If window has hash, it means we've got some FitBit api data to crunch!
    if(window.location.hash){
        url = window.location.hash.substr(1).split('&');
